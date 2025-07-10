6665bb621ff858f882c9cdd34fba6a634aaaf606
omar temsah <omartemsah99@gmail.com>
2025-07-11 02:02:30 +0300

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <Box className="max-w-xl">
+        <Skeleton />
+        <Flex gap="3" my="2">
+          <Skeleton width="5rem" />
+          <Skeleton width="8rem" />
+        </Flex>
+        <Card
+          className="prose prose-strong:text-zinc-200 prose-h1:text-zinc-200 text-zinc-200"
+          mt="4">
+          <Skeleton count={3} />
+        </Card>
+      </Box>
+    </SkeletonTheme>
+  );
 };
 
 export default LoadingIssueDetailPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Box } from "@radix-ui/themes";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box>
+      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+        <Skeleton />
+        <Skeleton height="20rem" />
+      </SkeletonTheme>
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
