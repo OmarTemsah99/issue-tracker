c59a9fb41ac3bf2669a32955f1fa4ebe18d873d3
omar temsah <omartemsah99@gmail.com>
2025-07-12 16:02:18 +0300

    ignoring generated file for eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "app/generated/**/*",
+      "prisma/generated/**/*",
+      "node_modules/**/*",
+      ".next/**/*",
+      "dist/**/*",
+      "build/**/*",
+    ],
+  },
 ];
 
 export default eslintConfig;
